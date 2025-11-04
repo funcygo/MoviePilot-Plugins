1a21b098ded353d2807a44d0017098c35ccc45d5
funcygo <707817519@qq.com>
2025-11-05 00:53:33 +0800

    Bump plugin version from 1.7 to 1.8123

--- plugins/zvideohelper/__init__.py
+++ plugins/zvideohelper/__init__.py
@@ -31,7 +31,7 @@ class ZvideoHelper(_PluginBase):
     # 插件图标
     plugin_icon = "zvideo.png"
     # 插件版本
-    plugin_version = "1.7"
+    plugin_version = "1.8"
     # 插件作者
     plugin_author = "funcygo"
     # 作者主页
